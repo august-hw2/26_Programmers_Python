ced39745cf8f32e4f70c995406a168ec9acf160b
august <55475872+august-hw2@users.noreply.github.com>
2026-08-07 13:55:11 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,5 +1,5 @@
 def solution(my_string, overwrite_string, s):
-    answer = my_string[0:s]+overwrite_string[:len(overwrite_string)]+my_string[s+len(overwrite_string):]
+    answer = my_string[0:s]+overwrite_string+my_string[s+len(overwrite_string):]
     
     
     
